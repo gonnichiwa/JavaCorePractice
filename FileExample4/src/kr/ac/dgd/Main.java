9eb955d52d50ff78395c5b31cb54482175fe00ed
gonnichiwa <keepthefaith00@gmail.com>
2021-08-13 11:50:01 +0900

    FileExample4 예제 추가

--- FileExample4/src/kr/ac/dgd/Main.java
+++ FileExample4/src/kr/ac/dgd/Main.java
@@ -1,6 +1,5 @@
 package kr.ac.dgd;
 
-import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
@@ -9,12 +8,15 @@ public class Main {
 
     public static void main(String[] args) {
 //        FileExample4 : 485p
+        // java 코드에서 생성한 문자열 데이터를 FileOutputStream 이용하여 fileOutput.txt에 쓰기.
         FileOutputStream fos = null;
-        String msg = "FileOutputStream Test";
-        byte[] byteArray = msg.getBytes(StandardCharsets.UTF_8);
+        String msg = "FileOutputStream Test 안녕하세요";
+        byte[] byteArray = msg.getBytes(StandardCharsets.UTF_8); // 안전하게 UTF-8인코딩 지정하여 다국어(한글) 지원 할 수 있도록.
 
         try {
-            fos = new FileOutputStream("/fileOutput.txt", true);
+            // 상대경로를 지정할때 어떻게 테스트 해가면서 쓸지? : ./ 찍어서 실제 동작이 어떻게 되는지 확인하고 작성.
+            // apppend의 뜻. : 기존 컨텐츠에 ++.
+            fos = new FileOutputStream("./FileExample4/demo/fileOutput.txt", false);
             fos.write(byteArray);
         } catch (IOException e) {
             e.printStackTrace();
